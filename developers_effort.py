b39608e940c7969e637f8496b0a90f03b0d4dd49
EarlGibe <g.tabarellidefatis-1@studenti.unitn.it>
2024-09-24 19:10:47 +0300

    Initialize developers effort task

---
+++ developers_effort.py
@@ -0,0 +1,14 @@
+"""Module for task e"""
+
+
+class DevEffort:
+    """Class for developers effort"""
+
+    def __init__(self, project):
+        self.project = project
+
+# INPUT: repository name and commit hashes
+# go inside local repository
+# git show <commit>
+# get author name
+# SCC: touched lines of code for each developer
