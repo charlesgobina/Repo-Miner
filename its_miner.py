7a244449eb4df5439c530676d3c7907df68c4d5d
uahmad417 <18u.ahmad@gmail.com>
2024-09-26 18:43:05 +0300

    created ITSMiner class to mine the github issue data

--- its_miner.py
+++ its_miner.py
@@ -1,1 +1,39 @@
-"""Mine the issue data from github repos"""
+"""Mines the issue data for a github repo"""
+
+from github import Auth, Github
+
+
+class ITSMiner:
+    """
+    Represents the issue data miner for github
+
+    Attributes:
+    ----------
+
+    __issue_data (dict): Class variable that stores all the issue data for a repo
+
+    Methods:
+    --------
+
+    get_issue_data(): Class method to get all the issue data for a repo
+    """
+
+    __issue_data: dict = {}
+
+    @classmethod
+    def mine_issue_data(repo: str) -> dict:
+        """
+        Mines all the issue data for a github repository
+
+        Parameter:
+        --------
+
+        repo (str): Name of the repository. Has to be of the format `Owner/Repository`
+
+        Returns:
+        -------
+
+        Returns a dictionary with all issue data
+        """
+
+        pass
