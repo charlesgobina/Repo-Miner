b7af645396a10c4ac9fa0123351ab93bcfd5c1a4
EarlGibe <g.tabarellidefatis-1@studenti.unitn.it>
2024-10-02 16:25:44 +0300

    Separate project and commit mining functions

--- developers_effort.py
+++ developers_effort.py
@@ -1,12 +1,15 @@
 """Developers effort component: collects total TLOC for each refactoring and developer"""
 
 import json
+import csv
 from pprint import pprint
+import datetime
 import git
-
 
 class DevEffort:
     """Class for developers effort; initialized with a GitHub project path"""
+
+    commit_details_matrix = []
 
     def __init__(self, json_path):
         # print(f"Reading JSON file: {json_path}")
@@ -24,13 +27,40 @@ class DevEffort:
         project_path = f"/home/taba/root/projects/{project}"
         return project_path
 
-    def collect_commit_details(self):
+    def collect_commit_details(self, project, project_repo):
         """Collects commit details: hash, author, date, lines changed, files changed"""
+        commit_details_list = []
+        # print(project_dir)
+        for commit_hash in project["commits"]:
+            # print(commit_hash)
+            # print(project_repo)
+            commit = project_repo.commit(commit_hash)
+            try:
+                commit = project_repo.commit(commit_hash)
+                commit_details = {
+                    # "project": project['project'],
+                    "commit_hash": commit_hash,
+                    "author": commit.author.name,
+                    "date": commit.committed_datetime.strftime("%Y-%m-%d %H:%M:%S"),
+                    "lines_changed": commit.stats.total['lines'],
+                    "files_changed": len(commit.stats.files)
+                }
+                commit_details_list.append(commit_details)
+
+            except ValueError as value_error:
+                print(
+                    f"Warning: Commit {commit_hash} could not be resolved. Skipping.")
+                print(f"Value Error: {value_error}")
+                return None
+        commit_details_list.reverse()
+        return commit_details_list
+
+    def mine_projects(self):
+        """Prints commit data"""
         json_data = self.get_json()
         # print(json_data)
 
         projects = []
-        commit_details_matrix = []
 
         for project in enumerate(json_data):
             projects.append({
@@ -41,48 +71,27 @@ class DevEffort:
         # projects.reverse()
 
         for project in projects:
-            print(project["project"])
-            project_dir = self.get_project_dir(project["project"])
+            # print(project["project"])
+            project_name = project["project"]
+            project_dir = self.get_project_dir(project_name)
             project_repo = git.Repo(project_dir)
-            commit_details_list = []
-            # print(project_dir)
-            for commit_hash in project["commits"]:
-                # print(commit_hash)
-                # print(project_repo)
-                commit = project_repo.commit(commit_hash)
-                try:
-                    commit = project_repo.commit(commit_hash)
-                    commit_details = {
-                        "project": project['project'],
-                        "commit_hash": commit_hash,
-                        "author": commit.author.name,
-                        # "date": commit.committed_datetime,
-                        "lines_changed": commit.stats.total['lines'],
-                        "files_changed": len(commit.stats.files)
-                    }
-                    commit_details_list.append(commit_details)
+            commit_details_list = self.collect_commit_details(project, project_repo)
 
-                except ValueError as value_error:
-                    print(
-                        f"Warning: Commit {commit_hash} could not be resolved. Skipping.")
-                    print(f"Value Error: {value_error}")
-                    return None
-                commit_details_matrix.append(commit_details_list)
-        return commit_details_matrix
+            pretty_print = 0
+            if pretty_print:
+                pprint(commit_details_list)
+            # print(len(commit_details_list))
 
-    def print_commit_data(self):
-        """Prints commit data"""
-        commit_details_list = self.collect_commit_details()
-        # pprint(commit_details_list)
-        print(len(commit_details_list))
-
-        with open('commit_details.json', 'w', encoding='utf-8') as cd_file:
-            json.dump(commit_details_list, cd_file)
-
-        return cd_file
+            with open(f"commit_details/{project_name}.json", 'w', encoding='utf-8') as cd_file:
+                json.dump(commit_details_list, cd_file)
 
     def organize_by_refactoring(self):
         """Organizes the output by refactoring"""
+        with open("dev_effort.csv", "w", newline="", encoding='utf-8') as csv_file:
+            for commit in self.commit_details_matrix:
+                dict_writer = csv.DictWriter(csv_file, commit.keys())
+                dict_writer.writeheader()
+                dict_writer.writerow(commit)
 
     def organize_by_developer(self):
         """Organizes the output by developer"""
--- effort_test.py
+++ effort_test.py
@@ -3,4 +3,5 @@
 from developers_effort import DevEffort
 
 developers_effort = DevEffort("commits_hash.json")
-developers_effort.print_commit_data()
+developers_effort.mine_projects()
+# developers_effort.organize_by_refactoring()
