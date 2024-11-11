48d064b94ac4ffdcff7a294f98fa3499ac2cc40a
charlesgobina <charles.gobina@gmail.com>
2024-11-11 21:01:37 +0200

    fix: fix developer effort file

--- developers_effort.py
+++ developers_effort.py
@@ -7,6 +7,7 @@ from pprint import pprint
 import subprocess
 import git
 
+
 class DevEffort:
     """Class for developers effort; initialized with a GitHub project path"""
 
@@ -25,7 +26,9 @@ class DevEffort:
 
     def get_project_dir(self, project):
         """Returns project directory"""
-        project_path = f"/home/taba/root/projects/{project}"
+        # get the current working directory
+        pwd = os.getcwd()
+        project_path = f"{pwd}/cloned_repos/{project}"
         return project_path
 
     def collect_commit_details(self, project, project_repo):
@@ -54,11 +57,22 @@ class DevEffort:
 
     def get_loc(self, directory, commit):
         """Switches current repository to specified commit"""
+
+        # get project name from directory
+        project_name = directory.split("/")[-1]
+
+        pwd_original = os.getcwd()
+        if not os.path.exists(f"{os.getcwd()}/developer_effort/{project_name}"):
+            os.makedirs(f"{os.getcwd()}/developer_effort/{project_name}")
+
         os.chdir(directory)
+
         subprocess.run(["git", "checkout", f"{commit}"], check=False)
         scc_file = f"scc_{commit}.json"
-        subprocess.run(["scc", "-f", "json", "-o", scc_file], check=False)
+        subprocess.run(["scc", "-f", "json", "-o",
+                       f'{pwd_original}/developer_effort/{project_name}/{scc_file}'], check=False)
 
+        os.chdir(f"{pwd_original}/developer_effort/{project_name}")
         loc_counter = 0
         with open(scc_file, 'r', encoding='utf-8') as scc_file:
             scc_data = json.load(scc_file)
@@ -73,26 +87,45 @@ class DevEffort:
 
         tloc_list = []
 
+        print("-------------------------------------------------")
+        print(current_directory)
+        print(project_dir)
+        print("-------------------------------------------------")  
+
         for commit in commit_details_list:
-            commit_hash = commit['commit_hash']          
+            commit_hash = commit['commit_hash']
+
+            print("Commit hash: ", commit_hash)
+            print(commit_hash)
             current_loc = self.get_loc(project_dir, commit_hash)
 
-            rev_parse_command = subprocess.check_output(["git", "rev-parse", f"{commit_hash}^1"])
-            previous_commit = rev_parse_command.decode('utf-8').strip()
-            previous_loc = self.get_loc(project_dir, previous_commit)
+            try:
+                rev_parse_command = subprocess.check_output(
+                    ["git", "rev-parse", f"{commit_hash}^1"], stderr=subprocess.STDOUT)
+                previous_commit = rev_parse_command.decode('utf-8').strip()
+                previous_loc = self.get_loc(project_dir, previous_commit)
+                tloc = abs(current_loc - previous_loc)
+            except subprocess.CalledProcessError as e:
+                print(
+                    f"Error getting previous commit for {commit_hash}: {e.output.decode('utf-8')}")
+                tloc = current_loc  # If there's no previous commit, consider the current LOC as TLOC
 
-            tloc = abs(current_loc - previous_loc)
             tloc_list.append(tloc)
 
         for tloc in tloc_list:
+            print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
             print(tloc)
 
+        print(current_directory)
         os.chdir(current_directory)
 
     def mine_projects(self):
         """Prints commit data"""
         json_data = self.get_json()
         # print(json_data)
+        print("###########################___________________")
+        print(os.getcwd())
+        print("###########################___________________")
 
         projects = []
 
@@ -107,18 +140,25 @@ class DevEffort:
             # print(project["project"])
             project_name = project["project"]
             project_dir = self.get_project_dir(project_name)
+            print(project_dir)
             project_repo = git.Repo(project_dir)
 
-            commit_details_list = self.collect_commit_details(project, project_repo)
-            sorted_commit_details = sorted(commit_details_list, key=itemgetter('date'))
+            commit_details_list = self.collect_commit_details(
+                project, project_repo)
+            sorted_commit_details = sorted(
+                commit_details_list, key=itemgetter('date'))
 
-            with open(f"commit_details/{project_name}.json", 'w', encoding='utf-8') as cd_file:
-                json.dump(sorted_commit_details, cd_file)
+            with open(f"{os.getcwd()}/developer_effort/{project_name}.json", 'w', encoding='utf-8') as cd_file:
+                json.dump(sorted_commit_details, cd_file, indent=4)
 
-            pretty_print = 1
+            print("############################################")
+            print("Commit details for project: ", sorted_commit_details)
+
+            pretty_print = 0
             if pretty_print:
                 pprint(sorted_commit_details)
-            # print(len(sorted_commit_details))
+            print(len(sorted_commit_details))
 
             current_directory = os.getcwd()
-            self.touched_lines_of_code(current_directory, project_dir, sorted_commit_details)
+            self.touched_lines_of_code(
+                current_directory, project_dir, sorted_commit_details)
